81ea9d89b523bd90948002545377f30dbd9884ae
Csaba Benko <csaba.benko@gmail.com>
2022-10-12 17:08:39 +0200

    Added window size constants.

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from turret import Turret
 
 class Game:
     def __init__(self):
+        self.WIDTH = 600
+        self.HEIGHT = 600
         self.game_over = False
 
         pygame.init()
-        self.surface = pygame.display.set_mode([600, 600])
+        self.surface = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
         pygame.display.set_caption("PySplat")
         self.surface.fill((0, 0, 0))
 
-        self.turret = Turret(600, 600)
+        self.turret = Turret(self.WIDTH, self.HEIGHT)
 
         self.main_loop()
 
